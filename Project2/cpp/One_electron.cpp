bbca1040a6ff7754886f8d85a3035d188748181c
Laura Beghini <laurabeg@student.matnat.uio.no>
2019-09-23 10:57:55 +0200

    Update One_electron.cpp

--- Project2/cpp/One_electron.cpp
+++ Project2/cpp/One_electron.cpp
@@ -34,62 +34,124 @@ int main(int argc, char* argv[]){
         tol = atof(argv[2]);
         tolstr = argv[2];
     }
-    int n;
-    cout << "Give next value of n (0 to end):" << endl;
-    cin >> n;
     
-    while(n != 0){
-        string outfile = fname;
-        //outfile.append("_"+to_string(n)+"_"+tolstr);
-        
-        mat A(n,n);
-        mat V(n,n);
-        double max_r;
-        double h;
-        vec r(n);
-        int interact = 0;
-        double wr = 0;
-        vec e_vals(n);
-        int n_eigenval=5;
-        vec eigenval(n_eigenval);
-        //mat e_vecs(3,n);
-        
-        if(n<n_eigenval){
-            cout<<"n must be bigger than 4"<<endl;
-            exit(1);
-        }
-        
-        cout<<"Give max. r/alpha:"<<endl;
-        cin>>max_r;
-        h=max_r/(n+1);
-        
-        initialize(n, h, A, r, V, interact, wr);
-        //cout<<A<<endl;
-        jacobi(n,tol,A,V);
-        //cout<<A<<endl;
-        e_vals=get_eigenvals(A,n);
-        //e_vecs=get_eigenvecs(A,V,n);
-        //cout<<e_vals;
-        
-        for(int i=0; i<n_eigenval; i++){
-            eigenval(i)=e_vals(i);
-        }
-        
-        //cout<<eigenval;
-        ofile.open(outfile+".txt");
-        ofile << setiosflags(ios::showpoint | ios::uppercase);
-        ofile << "r_max:" << max_r << endl;
-        ofile << "Jacobi eigenvalues:"<<endl;
-        for(int i=0; i<n_eigenval; i++){
-            ofile << setw(19) << setprecision(8) << eigenval[i] <<endl;
-        }
-        
-        ofile.close();
-        
-        cout << "Give next value of n (0 to end): " << endl;
-        cin >> n;
-    }
+
+    string outfile = fname;
+    //outfile.append("_"+to_string(n)+"_"+tolstr);
     
+    int selection;
+    cout<<"Write 1 to keep n constant and to vary r_max. Write 2 vice versa. Write 0 for the defaul program"<<endl;
+    cin>>selection;
+    
+    if(selection==0){
+            int n;
+            cout << "Give the value of n (0 to end):" << endl;
+            cin >> n;
+    
+            while(n != 0){
+        
+                mat A(n,n);
+                mat V(n,n);
+                double max_r;
+                double h;
+                vec r(n);
+                int interact = 0;
+                double wr = 0;
+                vec e_vals(n);
+                mat e_vecs(3,n);
+        
+                cout<<"Give max. r/alpha:"<<endl;
+                cin>>max_r;
+                h=max_r/(n+1);
+        
+                initialize(n, h, A, r, V, interact, wr);
+                //cout<<A<<endl;
+                jacobi(n,tol,A,V);
+                //cout<<A<<endl;
+                e_vals=get_eigenvals(A,n);
+                e_vecs=get_eigenvecs(A,V,n);
+        
+                //cout<<eigenval;
+                ofile.open(outfile);
+                ofile << setiosflags(ios::showpoint | ios::uppercase);
+                ofile << "r_max:" << max_r << endl;
+                ofile << "n:" << n << endl;
+                ofile << "Jacobi eigenvalues:           v0:               v1:              v2:"<<endl;
+                for(int i=0; i<n; i++){
+                    ofile << setw(19) << setprecision(8) << e_vals[i];
+                    ofile << setw(19) << setprecision(8) << e_vecs(0,i);
+                    ofile << setw(19) << setprecision(8) << e_vecs(1,i);
+                    ofile << setw(19) << setprecision(8) << e_vecs(2,i) << endl;
+                }
+        
+                ofile.close();
+        
+                cout << "Give next value of n (0 to end): " << endl;
+                cin >> n;
+            }
+        }
+            
+            
+    if(selection==1){
+            
+            int n;
+            cout << "Give the value of n (0 to end):" << endl;
+            cin >> n;
+        
+            if(n==0) exit(1);
+        
+            mat A(n,n);
+            mat V(n,n);
+            double max_r;
+            double min_r;
+            double h;
+            vec r(n);
+            int interact = 0;
+            int r_interval=0;
+            double wr = 0;
+            vec r_rep(10);
+            vec e_vals(n);
+            mat e_vecs(3,n);
+            
+            cout<<"Give max. r/alpha:"<<endl;
+            cin>>max_r;
+            
+            cout<<"Give min. r/alpha:"<<endl;
+            cin>>min_r;
+            
+            r_interval=(max_r-min_r)/10;
+            r_rep(0)=min_r;
+            for(int i=1; i<10; i++){
+                r_rep(i)=r_rep(i-1)+r_interval;
+            }
+            
+            for (int j=0; j<10; j++){
+                h=r_rep(j)/(n+1);
+        
+                initialize(n, h, A, r, V, interact, wr);
+                jacobi(n,tol,A,V);
+                e_vals=get_eigenvals(A,n);
+                e_vecs=get_eigenvecs(A,V,n);
+                
+                string fileout = fname;
+                string argument = to_string(j);
+                fileout.append(argument);
+                
+                ofile.open(fileout);
+                ofile << setiosflags(ios::showpoint | ios::uppercase);
+                ofile << "r:" << r_rep[j] << endl;
+                ofile << "n:" << n << endl;
+                ofile << "Jacobi eigenvalues:           v0:               v1:             v2:"<<endl;
+                for(int i=0; i<n; i++){
+                    ofile << setw(19) << setprecision(8) << e_vals[i];
+                    ofile << setw(19) << setprecision(8) << e_vecs(0,i);
+                    ofile << setw(19) << setprecision(8) << e_vecs(1,i);
+                    ofile << setw(19) << setprecision(8) << e_vecs(2,i) << endl;
+                }
+        
+            ofile.close();
+            }
+        }
     
     return 0;
 }
