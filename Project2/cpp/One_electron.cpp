6a22287c1f133458a134538a778d8084fcacfeb9
Laura2305 <54085129+Laura2305@users.noreply.github.com>
2019-09-27 19:27:20 +0200

    Update One_electron.cpp

--- Project2/cpp/One_electron.cpp
+++ Project2/cpp/One_electron.cpp
@@ -19,12 +19,15 @@ using namespace arma;
 
 ofstream ofile;
 
+//Begin main program
 int main(int argc, char* argv[]){
     string fname;         //beginning of filename; filenames will be "fname_m"
     double tol;           //max. allowed off-diagonal value in Jabobi's method
     string tolstr;
     double time=0;
     int count=0;
+    
+    //We get the filename and the tolerance from the user
     if(argc <= 2){
         cout << "No filename and/or tolerance; read filename and tolerance for "
         "Jacobi method on the same line." << endl;
@@ -39,42 +42,49 @@ int main(int argc, char* argv[]){
     }
     
 
+    //Definition of the output file
     string outfile = fname;
-    //outfile.append("_"+to_string(n)+"_"+tolstr);
     
+    //We let the user choose if he wants to vary r_max or not
     int selection;
     cout<<"Write 1 to keep n constant and to vary r_max. Write 0 for the defaul program"<<endl;
     cin>>selection;
     
+    //Case in which we do not vary n
     if(selection==0){
             int n;
+            //Here the user chooses the number of points of the discretize solution
             cout << "Give the value of n (0 to end):" << endl;
             cin >> n;
-    
+        
+            //While cicle to make the user choose different values of n after the first iteration
             while(n != 0){
             
-                mat A(n,n);
-                mat V(n,n);
-                double max_r;
-                double h;
-                vec r(n);
+                //declaration of varibles
+                mat A(n,n); mat V(n,n); mat e_vecs(3,n);
+                double max_r; double h; double wr = 0;
+                vec r(n); vec e_vals(n);
                 int interact = 0;
-                double wr = 0;
-                vec e_vals(n);
-                mat e_vecs(3,n);
-        
+                
+                //Here the user chooses the value of r_max
                 cout<<"Give max. r/alpha:"<<endl;
                 cin>>max_r;
+                //Definition of the step lenght
                 h=max_r/(n+1);
         
+                //Initialization of the matrix (this function are written in the jacobi.cpp file)
                 initialize_schrodinger(n, h, A, r, V, interact, wr);
-                //cout<<A<<endl;
+                
+                //Diagonalization of the matrix (this function are written in the jacobi.cpp file)
                 jacobi(n,1000,tol,A,V,time,count);
                 //cout<<A<<endl;
+                
+                //Here we get the eigenvalues and eigenvectors (this function are written in the jacobi.cpp file)
                 e_vals=get_eigenvals(A,n);
                 e_vecs=get_eigenvecs(A,V,n);
         
-                //cout<<eigenval;
+                //Here we print the results on a file
+                //We open the file
                 ofile.open(outfile);
                 ofile << setiosflags(ios::showpoint | ios::uppercase);
                 ofile << "r_max:" << max_r << endl;
@@ -88,48 +98,49 @@ int main(int argc, char* argv[]){
                     ofile << setw(19) << setprecision(8) << e_vecs(2,i) << endl;
                 }
         
+                //We close the file
                 ofile.close();
         
+                //Here we let the user choose if he wants to run the program with a different value for n
                 cout << "Give next value of n (0 to end): " << endl;
                 cin >> n;
             }
         }
-            
-            
+           
+    //Case in which we vary n
     if(selection==1){
             
             int n;
+            //Here the user chooses the number of points of the discretize solution
             cout << "Give the value of n (0 to end):" << endl;
             cin >> n;
         
             if(n==0) exit(1);
         
-            mat A(n,n);
-            mat V(n,n);
-            double max_r;
-            double min_r;
-            double h;
-            vec r(n);
+            //declaration of varibles
+            mat A(n,n); mat V(n,n); mat e_vecs(3,n);
+            double max_r; double min_r; double h; double r_interval=0; double wr = 0;
+            vec r(n); vec r_rep(10); vec e_vals(n);
             int interact = 0;
-            double r_interval=0;
-            double wr = 0;
-            vec r_rep(10);
-            vec e_vals(n);
-            mat e_vecs(3,n);
             
+            //Here the user chooses the interval in which to vary r_max
             cout<<"Give max. r/alpha:"<<endl;
             cin>>max_r;
             
             cout<<"Give min. r/alpha:"<<endl;
             cin>>min_r;
             
+            //Definition of the step between two different values of r_max
             r_interval=(max_r-min_r)/double(10.0);
             r_rep(0)=min_r;
+            //Definition of the vector for the r_max values. For each therm of the vector we will do an iteration
             for(int i=1; i<10; i++){
                 r_rep(i)=r_rep(i-1)+r_interval;
             }
             
+            //Cicle on the the different values of r_max (we have defined 10 possible values fot this variable)
             for (int j=0; j<10; j++){
+                //Definition of the step lenght
                 h=r_rep(j)/(n+1);
         
                 initialize_schrodinger(n, h, A, r, V, interact, wr);
