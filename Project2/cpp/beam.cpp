8b3e184d197291d3760e881d28e80b5b3a78c0b7
krissvang <kristian.vangsnes@hotmail.com>
2019-09-30 10:52:37 +0200

    Added comments

--- Project2/cpp/beam.cpp
+++ Project2/cpp/beam.cpp
@@ -13,6 +13,7 @@ using namespace arma;
 using namespace std::chrono;
 
 int main(int argc, char** argv){
+  //Checks if enough inputs
   if(argc<=1){
     cout << "Please choose number of mesh points"<<endl;
     return 1;
@@ -24,7 +25,8 @@ int main(int argc, char** argv){
     double tol=1e-16;
 
     //Matrices and vectors needed
-    mat A(n,n); mat A2(n,n); mat V(n,n); vec r(n);
+    mat A(n,n); mat A2(n,n); mat V(n,n); vec r(n); 
+    mat e_vecs(3,n); vec jacobi_e_vals(n);
     double h =  1.0/(n+1); double wr = 0;
     int interact = 0;
     vec eigenval;
@@ -33,34 +35,29 @@ int main(int argc, char** argv){
     double *t_arma = new double(0);
     int count = 0;
 
-    
-
-
-    //Just testing the algorithms
-    initialize_beam(n, h, A, r, V, interact, wr);
-    initialize_beam(n, h, A2, r, V, interact, wr);
-
-    time_point<high_resolution_clock> start, end;
-
-    start = high_resolution_clock::now();
-    //run armadillo
-    eig_sym(eigenval, eigenvec, A2);
-    end = high_resolution_clock::now();
-
-    duration<double> elapsed = end-start;
-    *t_arma = elapsed.count();
-
-    jacobi(n,1000,tol,A,V,t_jacobi, count);  
-    vec jacobi_e_vals(n);
-    jacobi_e_vals = get_eigenvals(A,n);
-    mat e_vecs(3,n);
-    e_vecs = get_eigenvecs(A,V,n);
-
+    //Determines filename
     string filename = "beam_res_";
     filename.append(to_string(n)+".txt");
     
-    write_eigenpairs(jacobi_e_vals, e_vecs,   eigenval, t_jacobi, *t_arma, filename, count, n);
+    //Initializing the matrices
+    initialize_beam(n, h, A, r, V, interact, wr);
+    initialize_beam(n, h, A2, r, V, interact, wr);
+    //Taking the time
+    time_point<high_resolution_clock> start, end;
+    start = high_resolution_clock::now();
+    //run armadillo
+    eig_sym(eigenval, eigenvec, A2);
 
+    end = high_resolution_clock::now();
+    duration<double> elapsed = end-start;
+    *t_arma = elapsed.count();
+    //Runs Jacobi algorithm
+    jacobi(n,1000,tol,A,V,t_jacobi, count);  
+    //Get eigenpairs
+    jacobi_e_vals = get_eigenvals(A,n);
+    e_vecs = get_eigenvecs(A,V,n);
+    //Writes to file
+    write_eigenpairs(jacobi_e_vals, e_vecs,   eigenval, t_jacobi, *t_arma, filename, count, n);
     return 0;
   }
 }
--- Project2/cpp/qho_no_int.cpp
+++ Project2/cpp/qho_no_int.cpp
@@ -14,6 +14,7 @@ using namespace arma;
 using namespace std::chrono;
 
 int main(int argc, char** argv){
+  //Checks if enough inputs
     if(argc<=2){
     cout << "Please choose number of mesh points and a max radius."<<endl;
     return 1;
@@ -25,7 +26,8 @@ int main(int argc, char** argv){
     double tol=1e-10;
 
     //Matrices and vectors needed
-    mat A(n,n); mat A2(n,n); mat V(n,n); vec r(n);
+    mat A(n,n); mat A2(n,n); mat V(n,n); vec r(n); 
+    vec jacobi_e_vals(n); mat e_vecs(3,n);
     double max = atof(argv[2]);
     double h = max/(n+1); double wr = 0;
     int interact = 0;
@@ -34,32 +36,28 @@ int main(int argc, char** argv){
     double t_jacobi=0;
     double *t_arma = new double(0);
     int count = 0;
+    //Filename
+    string filename = "qho_no_int_res_";
+    filename.append(to_string(n)+".txt");
 
-    
 
-
-    //Just testing the algorithms
+    //Initializes the matrices
     initialize_schrodinger(n, h, A, r, V, interact, wr);
     initialize_schrodinger(n, h, A2, r, V, interact, wr);
     
     time_point<high_resolution_clock> start, end;
-
     start = high_resolution_clock::now();
-    //run armadillo
+    //Runs armadillo
     eig_sym(eigenval, eigenvec, A2);
     end = high_resolution_clock::now();
-
     duration<double> elapsed = end-start;
     *t_arma = elapsed.count();
+    //Runs Jacobi algorithm
     jacobi(n,1000,tol,A,V,t_jacobi, count);  
-    vec jacobi_e_vals(n);
+    //Gets the eigenpairs
     jacobi_e_vals = get_eigenvals(A,n);
-    mat e_vecs(3,n);
     e_vecs = get_eigenvecs(A,V,n);
-
-    string filename = "qho_no_int_res_";
-    filename.append(to_string(n)+".txt");
-    
+    //Writes to file
     write_eigenpairs(jacobi_e_vals, e_vecs,   eigenval, t_jacobi, *t_arma, filename, count, n);
     return 0;
   }
