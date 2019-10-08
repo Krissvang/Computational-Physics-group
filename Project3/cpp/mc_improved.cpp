a83f5cbef8e70ea6e7dcac8674d7353785bd15ad
Tore Klungland <tore.klungland@fys.uio.no>
2019-10-08 17:20:53 +0200

    Fixed mc_improved, renamed variable on mc_brute

--- Project3/cpp/mc_improved.cpp
+++ Project3/cpp/mc_improved.cpp
@@ -9,25 +9,22 @@ double improved_MC(double *);
 int main()
 {
      int n;
-     double x[6], y, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
+     double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
      printf("Read in the number of Monte-Carlo samples\n");
      scanf("%d", &n);
 
      double int_mc = 0.;  double variance = 0.;
      double sum_sigma= 0. ; long idum=-1 ;
-     double max_r=5.; // we fix the max size of the box to L=5
-     double volume=(4/3)*acos(-1.)*pow(max_r,3);
      double jacobi_det = 4*pow(acos(-1.),4.)*1/16;
 
 //   evaluate the integral with importance sampling
      for ( int i = 1;  i <= n; i++){
 //   x[] contains the random numbers for all dimensions
-       //Generate r=r1+r2 according to exponential dist., then "distribute"
-       //uniformly between r1 and r2:
-       y = ran0(&idum);
-       r = -log(1-y);
-       x[0] = r*ran0(&idum);
-       x[1] = r-x[0];
+       //Generate r1 and r2 according to exponential dist.
+       y1 = ran0(&idum);
+       y2 = ran0(&idum);
+       x[0] = -log(1-y1)/4.;   //-log(1-y1)/(2*alpha)
+       x[1] = -log(1-y2)/4.;   //-log(1-y2)/(2*alpha)
        for (int j = 2; j< 4; j++) {
            x[j]=acos(-1.)*ran0(&idum);   //pi*random
        }
@@ -42,7 +39,7 @@ int main()
      sum_sigma = sum_sigma/((double) n );
      variance=sum_sigma-int_mc*int_mc;
 //   final output
-     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, volume*sqrt(variance/n), volume*int_mc, 5*M_PI*M_PI/(16*16));
+     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, jacobi_det*sqrt(variance/n), jacobi_det*int_mc, 5*M_PI*M_PI/(16*16));
      return 0;
 }  // end of main program
 
@@ -52,10 +49,9 @@ double  improved_MC(double *x)
 {
    double alpha = 2.;
 // evaluate the different terms of the exponential
-   double exp1 = - 2*alpha * (x[0]+x[1]) ;
    double cosb = cos(x[2])*cos(x[3])+sin(x[2])*sin(x[3])*cos(x[4]-x[5]);
    double deno = sqrt(x[0]*x[0]+x[1]*x[1]-2*x[0]*x[1]*cosb);
-   double value = x[0]*x[0]*x[1]*x[1]*sin(x[2])*sin(x[3])*exp(exp1)/deno;
+   double value = x[0]*x[0]*x[1]*x[1]*sin(x[2])*sin(x[3])/deno;
    return value;
 
 } // end function for the integrand
