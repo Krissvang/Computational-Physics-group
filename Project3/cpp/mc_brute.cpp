c35c0ba0583b778b04a5bee619c57c5196cc7f70
Tore Klungland <tore.klungland@fys.uio.no>
2019-10-08 17:20:53 +0200

    Fixed mc_improved, renamed variable in mc_brute

--- Project3/cpp/mc_brute.cpp
+++ Project3/cpp/mc_brute.cpp
@@ -5,44 +5,44 @@
 using namespace std;
 
 double brute_force_MC(double *);
-//     Main function begins here     
+//     Main function begins here
 int main()
 {
      int n;
-     double x[6], y, fx; 
+     double x[6], y, fx;
      printf("Read in the number of Monte-Carlo samples\n");
      scanf("%d", &n);
 
      double int_mc = 0.;  double variance = 0.;
-     double sum_sigma= 0. ; long idum=-1 ;  
-     double length=5.; // we fix the max size of the box to L=5
-     double volume=pow((2*length),6);
+     double sum_sigma= 0. ; long idum=-1 ;
+     double length=10.; // we fix the max size of the box to L=20
+     double jacobi_det=pow((2*length),6);
 
-//   evaluate the integral with importance sampling    
+//   evaluate the integral with importance sampling
      for ( int i = 1;  i <= n; i++){
 //   x[] contains the random numbers for all dimensions
        for (int j = 0; j< 6; j++) {
            x[j]=-length+2*length*ran0(&idum);
        }
-       fx=brute_force_MC(x); 
+       fx=brute_force_MC(x);
        int_mc += fx;
        sum_sigma += fx*fx;
      }
      int_mc = int_mc/((double) n );
      sum_sigma = sum_sigma/((double) n );
      variance=sum_sigma-int_mc*int_mc;
-//   final output 
-     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, volume*sqrt(variance/n), volume*int_mc, 5*M_PI*M_PI/(16*16)); 
+//   final output
+     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, jacobi_det*sqrt(variance/n), jacobi_det*int_mc, 5*M_PI*M_PI/(16*16));
      return 0;
-}  // end of main program 
+}  // end of main program
 
-// this function defines the integrand to integrate  
-double  brute_force_MC(double *x) 
+// this function defines the integrand to integrate
+double  brute_force_MC(double *x)
 {
    double alpha = 2.;
 // evaluate the different terms of the exponential
    double exp1=-2*alpha*sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
    double exp2=-2*alpha*sqrt(x[3]*x[3]+x[4]*x[4]+x[5]*x[5]);;
    double deno = sqrt(pow((x[0]-x[3]),2)+pow((x[1]-x[4]),2)+pow((x[2]-x[5]),2));
-   return exp(exp1+exp2)/deno; 
+   return exp(exp1+exp2)/deno;
 } // end function for the integrand
--- Project3/cpp/mc_improved.cpp
+++ Project3/cpp/mc_improved.cpp
@@ -5,57 +5,53 @@
 using namespace std;
 
 double improved_MC(double *);
-//     Main function begins here     
+//     Main function begins here
 int main()
 {
      int n;
-     double x[6], y, fx; 
+     double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
      printf("Read in the number of Monte-Carlo samples\n");
      scanf("%d", &n);
 
      double int_mc = 0.;  double variance = 0.;
-     double sum_sigma= 0. ; long idum=-1 ;  
-     double max_r=1.; // we fix the max size of the box to L=5
-     double volume=(4/3)*acos(-1.)*pow(max_r,3);
+     double sum_sigma= 0. ; long idum=-1 ;
      double jacobi_det = 4*pow(acos(-1.),4.)*1/16;
 
-//   evaluate the integral with importance sampling    
+//   evaluate the integral with importance sampling
      for ( int i = 1;  i <= n; i++){
 //   x[] contains the random numbers for all dimensions
-       for (int j = 0; j< 2; j++) {
-           y=ran0(&idum)*(1-exp(-max_r));
-           x[j]=-log(1.-y);
-           cout << x[j] << endl;
-           
-       }
+       //Generate r1 and r2 according to exponential dist.
+       y1 = ran0(&idum);
+       y2 = ran0(&idum);
+       x[0] = -log(1-y1)/4.;   //-log(1-y1)/(2*alpha)
+       x[1] = -log(1-y2)/4.;   //-log(1-y2)/(2*alpha)
        for (int j = 2; j< 4; j++) {
-           x[j]=2*acos(-1.)*ran0(&idum);  //2*pi*random
+           x[j]=acos(-1.)*ran0(&idum);   //pi*random
        }
        for (int j = 4; j< 6; j++) {
-           x[j]=acos(-1.)*ran0(&idum);    
+           x[j]=2*acos(-1.)*ran0(&idum); //2*pi*random
        }
-       fx=improved_MC(x); 
+       fx=improved_MC(x);
        int_mc += fx;
        sum_sigma += fx*fx;
      }
      int_mc = int_mc/((double) n );
      sum_sigma = sum_sigma/((double) n );
      variance=sum_sigma-int_mc*int_mc;
-//   final output 
-     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, volume*sqrt(variance/n), volume*int_mc, 5*M_PI*M_PI/(16*16)); 
+//   final output
+     printf("%d standard deviation= %12.5E, Inum= %12.5E, exact= %f", n, jacobi_det*sqrt(variance/n), jacobi_det*int_mc, 5*M_PI*M_PI/(16*16));
      return 0;
-}  // end of main program 
+}  // end of main program
 
-// this function defines the integrand to integrate 
- 
-double  improved_MC(double *x) 
+// this function defines the integrand to integrate
+
+double  improved_MC(double *x)
 {
    double alpha = 2.;
 // evaluate the different terms of the exponential
-   double exp1 = - alpha * (x[0]+x[1]) ;
    double cosb = cos(x[2])*cos(x[3])+sin(x[2])*sin(x[3])*cos(x[4]-x[5]);
    double deno = sqrt(x[0]*x[0]+x[1]*x[1]-2*x[0]*x[1]*cosb);
-   double value = x[0]*x[0]*x[1]*x[1]*sin(x[2])*sin(x[3])*exp(exp1)/deno;
+   double value = x[0]*x[0]*x[1]*x[1]*sin(x[2])*sin(x[3])/deno;
    return value;
-   
+
 } // end function for the integrand
