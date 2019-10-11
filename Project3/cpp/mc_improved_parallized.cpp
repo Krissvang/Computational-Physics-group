cd6a19deb4427889e47df047854774abbfe7992c
Tore Klungland <tore.klungland@fys.uio.no>
2019-10-11 23:21:03 +0200

    Used montecarlo.h in parallelization program, removed output from montecarlo.cpp

--- Project3/cpp/mc_improved_parallized.cpp
+++ Project3/cpp/mc_improved_parallized.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <mpi.h>
 #include <time.h>
+#include "montecarlo.h"
 using namespace std;
 using namespace std::chrono;
 
@@ -18,63 +19,41 @@ int main(int nargs, char* args[])
      time2 = system_clock::now();
      duration<double> duration_in_seconds =duration<double>(time2.time_since_epoch());
      long t2= duration_in_seconds.count();
-     int numprocs, my_rank, i, n;
-     n=10000000;
+     int numprocs, my_rank, i;
      MPI_Init (&nargs, &args);
      MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
      MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
-     int local_n=n/numprocs;
-     double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
-
-     double int_mc = 0.;  double variance = 0.;
-     double local_int_mc=0.; double local_variance=0.;
-     double sum_sigma= 0. ; long idum=t2+my_rank;
-     double local_sum_sigma=0.;
-     double jacobi_det = 4*pow(acos(-1.),4.)*1/16;
-
-//   evaluate the integral with importance sampling
-     for ( int i = 1;  i <= local_n; i++){
-//   x[] contains the random numbers for all dimensions
-       //Generate r1 and r2 according to exponential dist.
-       y1 = ran0(&idum);
-       y2 = ran0(&idum);
-       x[0] = -log(1-y1)/4.;   //-log(1-y1)/(2*alpha)
-       x[1] = -log(1-y2)/4.;   //-log(1-y2)/(2*alpha)
-       for (int j = 2; j< 4; j++) {
-           x[j]=acos(-1.)*ran0(&idum);   //pi*random
+     int n;
+     if(nargs < 2){
+       if(my_rank == 0){
+         cout << "Please read in number of Monte Carlo "
+         "samples on the same line." << endl;
        }
-       for (int j = 4; j< 6; j++) {
-           x[j]=2*acos(-1.)*ran0(&idum); //2*pi*random
-       }
-       fx=improved_MC(x);
-       local_int_mc += fx;
-       local_sum_sigma += fx*fx;
+       exit(1);
      }
-     local_int_mc = local_int_mc;
-     local_sum_sigma = local_sum_sigma;
-     local_variance=local_sum_sigma-local_int_mc*local_int_mc;
+     else n = atoi(args[1]);
+     int local_n=n/numprocs;
+     double int_mc, std_dev, local_int_mc, local_std_dev, local_time;
+     mc_improved(&improved_MC,local_n,local_int_mc,local_std_dev,local_time);
      MPI_Reduce(&local_int_mc, &int_mc, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-     MPI_Reduce(&local_sum_sigma, &sum_sigma, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-     int_mc*=1./n;
-     sum_sigma*=1./n;
-     variance = sum_sigma-int_mc*int_mc;
+     MPI_Reduce(&local_std_dev, &std_dev, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+     int_mc/=numprocs;
+     std_dev/=numprocs*sqrt(numprocs);
      end = high_resolution_clock::now();
      duration<double> elapsed = end-start;
      double time = elapsed.count();
     //   final output
      if( my_rank==0){
-     cout << "Standard deviation = "<< jacobi_det*sqrt(variance/n) <<  " Integral = " << jacobi_det*int_mc << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl; 
+     cout << "Standard deviation = "<< std_dev <<  " Integral = " << int_mc <<
+     " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl;
      }
      MPI_Finalize();
      return 0;
 }  // end of main program
 
 // this function defines the integrand to integrate
-
 double  improved_MC(double *x)
 {
-   double alpha = 2.;
-// evaluate the different terms of the exponential
    double cosb = cos(x[2])*cos(x[3])+sin(x[2])*sin(x[3])*cos(x[4]-x[5]);
    double deno = sqrt(x[0]*x[0]+x[1]*x[1]-2*x[0]*x[1]*cosb);
    double value = x[0]*x[0]*x[1]*x[1]*sin(x[2])*sin(x[3])/deno;
--- Project3/cpp/montecarlo.cpp
+++ Project3/cpp/montecarlo.cpp
@@ -35,8 +35,6 @@ void mc_bruteforce(double (*func) (double *), int n, double R, double& int_mc, d
   time = elapsed.count();
   int_mc *= jacobi_det;
   std_dev = jacobi_det*sqrt(variance/n);
-//   final output
-  cout << "Standard deviation=" << std_dev <<" Integral= " << int_mc <<" Exact= " << 5*M_PI*M_PI/(16*16) << " Time = "<< time << endl;
 }
 
 void mc_improved(double (*func) (double *), int n, double& int_mc, double& std_dev, double& time){
@@ -77,6 +75,4 @@ void mc_improved(double (*func) (double *), int n, double& int_mc, double& std_d
   time = elapsed.count();
   int_mc *= jacobi_det;
   std_dev = jacobi_det*sqrt(variance/n);
-//   final output
-  cout << "Standard deviation = "<< std_dev <<  " Integral = " << int_mc << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl;
 }
