8ea1d0363c5242eb4f69b1ca82a1d25bcadf16df
Tore Klungland <tore.klungland@fys.uio.no>
2019-10-11 15:26:56 +0200

    Fixed bug in mc_improved_parallized

--- Project3/cpp/mc_improved_parallized.cpp
+++ Project3/cpp/mc_improved_parallized.cpp
@@ -19,15 +19,15 @@ int main(int nargs, char* args[])
      MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
      MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
      int local_n=n/numprocs;
-     
+
      double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
-     
+
      double int_mc = 0.;  double variance = 0.;
      double local_int_mc=0.; double local_variance=0.;
      double sum_sigma= 0. ; long idum=-1 ;
      double local_sum_sigma=0.;
      double jacobi_det = 4*pow(acos(-1.),4.)*1/16;
-    
+
 //   evaluate the integral with importance sampling
      for ( int i = 1;  i <= local_n; i++){
 //   x[] contains the random numbers for all dimensions
@@ -55,7 +55,7 @@ int main(int nargs, char* args[])
      double time = elapsed.count();
     //   final output
      if( my_rank==0){
-     cout << "Standard deviation = "<< jacobi_det*sqrt(variance/n) <<  " Integral = " << jacobi_det*int_mc/4 << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl; 
+     cout << "Standard deviation = "<< jacobi_det*sqrt(variance/n) <<  " Integral = " << jacobi_det*int_mc/numprocs << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl; 
      }
      MPI_Finalize();
      return 0;
