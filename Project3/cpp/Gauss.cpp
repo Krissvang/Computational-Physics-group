20ffcddb60622705883bf8443492a37d6f367e4f
Laura Beghini <laurabeg@student.matnat.uio.no>
2019-10-11 12:28:01 +0200

    Update Gauss.cpp

--- Project3/cpp/Gauss.cpp
+++ Project3/cpp/Gauss.cpp
@@ -19,8 +19,13 @@ using namespace std;
 //     Here we define various functions called by the main program
 
 double int_function(double x1, double y1, double z1, double x2, double y2, double z2);
+double int_improved_function(double r1, double theta1, double phi1, double r2, double theta2, double phi2);
 double one_electron_function(double x);
 void gauleg(double x1, double x2, double x[], double w[], int n);
+void gauss_laguerre(double *x, double *w, int n, double alf);
+double gammln( double xx);
+
+
 
 
 //   Main function begins here
@@ -32,10 +37,10 @@ int main()
     double a, b;
     int selection;
     
-    cout << "Insert 1 if you want to evaluate the one electron function, insert 0 if you want to solve the integral" << endl;
+    cout << "Insert 0 if you want to evaluate the one electron function. insert 1 if you want to solve the integral with the Gauss-Legendre algorithm. Write 2 if you want to use the Improved Gauss-Quadrature." << endl;
     cin >> selection;
     
-    if(selection==1){
+    if(selection==0){
         cout << "Read in the number of points" << endl;
         cin >> n;
         cout << "Read the function limits" << endl;
@@ -68,7 +73,7 @@ int main()
         delete [] f;
     }
     
-    if(selection==0){
+    if(selection==1){
         int N;
         cout << "Read in the number of integration points" << endl;
         cin >> N;
@@ -102,6 +107,7 @@ int main()
         
         ofile.open(outfile);
         ofile << setiosflags(ios::showpoint | ios::uppercase);
+        ofile << "GAUSS-LEGENDRE QUADRATURE:" << endl;
         ofile << "number of points:" << endl;
         ofile << N << endl;
         ofile << "integration limits:" << endl;
@@ -113,22 +119,89 @@ int main()
         delete [] x;
         delete [] w;
     }
+    
+    if(selection==2){
+        int N;
+        cout << "Read in the number of integration points" << endl;
+        cin >> N;
+        cout << "Write the output file name" <<endl;
+        cin >> outfile;
+        
+        double alf = 1.0;
+        
+        //reserve space in memory for vectors containing the mesh points
+        //weights and function values for the use of the gauss-laguerre
+        //method
+        // Gauss-Laguerre is old-fashioned translation of F77 --> C++
+        // arrays start at 1 and end at n
+        double *x_theta = new double [N];
+        double *x_phi = new double [N];
+        double *x_r = new double [N+1];
+        double *w_theta = new double [N];
+        double *w_phi = new double [N];
+        double *w_r = new double [N+1];
+        
+        double a_theta = 0;
+        double b_theta = 3.14159265359;
+        double a_phi = 0;
+        double b_phi = 2*3.14159265359;
+        
+        //set up the mesh points and weights for the angular part
+        gauleg(a_theta,b_theta,x_theta,w_theta, N);
+        gauleg(a_phi,b_phi,x_phi,w_phi, N);
+        
+        //   set up the mesh points and weights and the power of x^alf
+        double alf = 2.0;
+        gauss_laguerre(x_r,w_r, N, alf);
+        
+        //   evaluate the integral with the Gauss-Laguerre method
+        //   Note that we initialize the sum
+        double int_gausslag = 0.;
+        
+        
+        
+        
+        ofile.open(outfile);
+        ofile << setiosflags(ios::showpoint | ios::uppercase);
+        ofile << "IMPROVED GAUSS-QUADRATURE:" << endl;
+        ofile << "number of points:" << endl;
+        ofile << N << endl;
+        ofile << "integral result:" <<endl;
+        ofile << int_gauss << endl;
+        
+        
+        delete [] x_theta;
+        delete [] x_phi;
+        delete [] x_r;
+        delete [] w_theta;
+        delete [] w_phi;
+        delete [] w_r;
+        
+    }
 
     return 0;
 }  // end of main program
 
-//  this function defines the function to integrate
+//  this function defines the function to integrate with the Gauss-Legendre quadrature
 double int_function(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-   double alpha = 2.;
 // evaluate the different terms of the exponential
-    double exp1=-2*alpha*sqrt(x1*x1+y1*y1+z1*z1);
-    double exp2=-2*alpha*sqrt(x2*x2+y2*y2+z2*z2);
+    double exp1=-4*sqrt(x1*x1+y1*y1+z1*z1);
+    double exp2=-4*sqrt(x2*x2+y2*y2+z2*z2);
     double deno=sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
     if(deno<1e-10) return 0;
     else return exp(exp1+exp2)/deno;
 } // end of function to evaluate
 
+
+//  this function defines the function to integrate with the improved Gauss Quadrature
+double int_improved_function(double r1, double theta1, double phi1, double r2, double theta2, double phi2)
+{
+    beta=cos(theta1)*cos(theta2)+sin(theta1)*sin(theta2)*cos(phi1-phi2);
+    double deno=sqrt(pow(r1,2)+pow(r2,2)-2*r1*r2*beta);
+    if(deno<1e-10) return 0;
+    else return exp(-3*(r1+r2))/deno;
+} // end of function to evaluate
 
 
 //  this function defines the wave function of one electron
@@ -208,3 +281,61 @@ void gauleg(double x1, double x2, double x[], double w[], int n)
       *(w_high--) = *(w_low++);
    }
 } // End_ function gauleg()
+
+
+//  Note that you need to call it with a given value of alpha,
+// called alf here. This comes from x^{alpha} exp(-x)
+
+void gauss_laguerre(double *x, double *w, int n, double alf)
+{
+    int i,its,j;
+    double ai;
+    double p1,p2,p3,pp,z,z1;
+
+    for (i=1;i<=n;i++) {
+        if (i == 1) {
+            z=(1.0+alf)*(3.0+0.92*alf)/(1.0+2.4*n+1.8*alf);
+        } else if (i == 2) {
+            z += (15.0+6.25*alf)/(1.0+0.9*alf+2.5*n);
+        } else {
+            ai=i-2;
+            z += ((1.0+2.55*ai)/(1.9*ai)+1.26*ai*alf/
+                (1.0+3.5*ai))*(z-x[i-2])/(1.0+0.3*alf);
+        }
+        for (its=1;its<=MAXIT;its++) {
+            p1=1.0;
+            p2=0.0;
+            for (j=1;j<=n;j++) {
+                p3=p2;
+                p2=p1;
+                p1=((2*j-1+alf-z)*p2-(j-1+alf)*p3)/j;
+            }
+            pp=(n*p1-(n+alf)*p2)/z;
+            z1=z;
+            z=z1-p1/pp;
+            if (fabs(z-z1) <= EPS) break;
+        }
+        if (its > MAXIT) cout << "too many iterations in gaulag" << endl;
+        x[i]=z;
+        w[i] = -exp(gammln(alf+n)-gammln((double)n))/(pp*n*p2);
+    }
+}
+// end function gaulag
+
+double gammln( double xx)
+{
+    double x,y,tmp,ser;
+    static double cof[6]={76.18009172947146,-86.50532032941677,
+        24.01409824083091,-1.231739572450155,
+        0.1208650973866179e-2,-0.5395239384953e-5};
+    int j;
+
+    y=x=xx;
+    tmp=x+5.5;
+    tmp -= (x+0.5)*log(tmp);
+    ser=1.000000000190015;
+    for (j=0;j<=5;j++) ser += cof[j]/++y;
+    return -tmp+log(2.5066282746310005*ser/x);
+}
+
+// end function gammln
