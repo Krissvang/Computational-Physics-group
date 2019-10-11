975748d3c549cbfa715fc4b0441ec55559cb8870
krissvang <kristian.vangsnes@hotmail.com>
2019-10-11 15:21:32 +0200

    changeded parallized

--- Project3/cpp/mc_improved_parallized.cpp
+++ Project3/cpp/mc_improved_parallized.cpp
@@ -9,26 +9,25 @@ using namespace std::chrono;
 
 double improved_MC(double *);
 //     Main function begins here
-int main(int args, char* args[])
+int main(int nargs, char* args[])
 {
      time_point<high_resolution_clock> start, end;
      start = high_resolution_clock::now();
-     int numprocs, my_rank, i;
-     MPI_Init (&args, &args);
+     int numprocs, my_rank, i, n;
+     n=100000000;
+     MPI_Init (&nargs, &args);
      MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
      MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
-     double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
-     cout << "Read in the number of Monte-Carlo samples"<< endl;
-     cin >> n;
      int local_n=n/numprocs;
-
+     
+     double x[6], y1, y2, r, fx; //x = [r1,r2,theta1,theta2,phi1,phi2]
      
      double int_mc = 0.;  double variance = 0.;
      double local_int_mc=0.; double local_variance=0.;
      double sum_sigma= 0. ; long idum=-1 ;
      double local_sum_sigma=0.;
      double jacobi_det = 4*pow(acos(-1.),4.)*1/16;
-
+    
 //   evaluate the integral with importance sampling
      for ( int i = 1;  i <= local_n; i++){
 //   x[] contains the random numbers for all dimensions
@@ -54,8 +53,10 @@ int main(int args, char* args[])
      end = high_resolution_clock::now();
      duration<double> elapsed = end-start;
      double time = elapsed.count();
-//   final output
-     cout << "Standard deviation = "<< jacobi_det*sqrt(variance/n) <<  " Integral = " << jacobi_det*int_mc << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl;
+    //   final output
+     if( my_rank==0){
+     cout << "Standard deviation = "<< jacobi_det*sqrt(variance/n) <<  " Integral = " << jacobi_det*int_mc/4 << " exact= " << 5*M_PI*M_PI/(16*16) << " Time = " << time << endl; 
+     }
      MPI_Finalize();
      return 0;
 }  // end of main program
