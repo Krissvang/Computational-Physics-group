f39e0c6e4b51ab64b39a1fd0b08c8c985e14d834
Krissvang <kristian.vangsnes@hotmail.com>
2019-11-01 14:36:49 +0100

    Revert "Addet comments to plot.py"
    This reverts commit a40ffdfeb16d7644f6a3446b9660004bf0c7a57c.

--- Project4/cpp/plot.py
+++ Project4/cpp/plot.py
@@ -3,8 +3,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-#Run the c++ file most_likely_state.cpp and call the file most_likely_state_T=#temperature
-#Look at the reults folder for examples.
 
 def plot_mcs_vs_energy(temperature):
   mcs, energy, heatcap, mag, absmag, sucept, time = np.loadtxt("results/most_likely_state_T=%s.txt"%(temperature), skiprows=2,unpack=True) 
@@ -24,7 +22,7 @@ def plot_mcs_vs_mag(temperature):
   plt.savefig("results/mcs_vs_magnetization_T=%s.png"%(temperature))
   plt.close()
 
-#As argument choose the temperature
-#plot_mcs_vs_mag("1")
-#plot_mcs_vs_energy("1")
+
+plot_mcs_vs_mag("1")
+plot_mcs_vs_energy("1")
 
