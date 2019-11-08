2b7e039f1a33734810e88c2765fb651cdc455396
Laura Beghini <laurabeg@student.matnat.uio.no>
2019-11-08 12:56:06 +0100

    Update ising.h

--- Project4/cpp/ising.h
+++ Project4/cpp/ising.h
@@ -2,7 +2,12 @@
 #define	ISING_H
 
 #include <vector>
+#include <armadillo>
 using namespace std;
 
 void ising(int, int, double, string, double&, double&, double&, double&, double&, vector<int>&, int, int&);
+// Function to initialize energy and magnetization
+void initialize(int, string, double, arma::Mat<int>&, double&, double&, vector<int>&);
+// The metropolis algorithm
+void Metropolis(int, arma::Mat<int>&, double&, double&, double *, int&);
 #endif /* ISING_H */
