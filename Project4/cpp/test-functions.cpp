0ebe1e2f565a20310e70f4f9a0ede74ec14955c9
Laura Beghini <laurabeg@student.matnat.uio.no>
2019-11-08 12:00:51 +0100

    Update test-functions.cpp

--- Project4/cpp/test-functions.cpp
+++ Project4/cpp/test-functions.cpp
@@ -2,29 +2,26 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <chrono>
 #include "catch.hpp"
-#include "ising.h"
-#include <random>
-#include <armadillo>
+#include "ising.h"còe
 using namespace std;
-using namespace arma;
 
 /*TEST_CASE("Testing the ising model"){
-    int n_spins=2; int mcs=1000000;
+    int n_spins=2; int mcs=1000000; int steady_start=20;  int count_configs = 0;
+    vector<int> count(n_spins * n_spins + 1);
     double T=1.0;
     double E_avg, heatcap, M_avg, M_abs_avg, susc;
-    ising(n_spins,mcs,T,"r",E_avg,heatcap,M_avg,M_abs_avg,susc);
+    ising(n_spins,mcs,T,"r",E_avg,heatcap,M_avg,M_abs_avg,susc,count,steady_start,count_configs);
     double Z=12+2*exp(8/T)+2*exp(-8/T);
     REQUIRE(E_avg == Approx((16*exp(-8/T)-16*exp(8/T))/Z).epsilon(0.05));
     REQUIRE(heatcap == Approx(1/(T*T)*(64*(1+3*cosh(8/T)))/(3+cosh(8/T))/(3+cosh(8/T))).epsilon(0.08));
     REQUIRE(susc == Approx(1/T*(32*exp(8/T)+32)/Z).epsilon(0.05));
     REQUIRE(M_abs_avg == Approx((8*exp(8/T)+16)/Z).epsilon(0.05));
-    REQUIRE(M_avg == Approx(0.0).epsilon(0.4));
+    REQUIRE(M_avg+1 == Approx(1).epsilon(0.7));
 }*/
 
 TEST_CASE("Testing the metropolis algorithm"){
-    int n_spins=2;
+    int n_spins=2; int count_configs = 0;
     double T=0.00001;
     Mat<int> spin_matrix(n_spins,n_spins);
     vector<int> count;
@@ -33,7 +30,7 @@ TEST_CASE("Testing the metropolis algorithm"){
     for( int de =-8; de <= 8; de++) exp_de[de+8] = 0;
     for( int de =-8; de <= 8; de+=4) exp_de[de+8] = exp(-de/T);
     initialize(n_spins, "a", T, spin_matrix, E, M, count);
-    Metropolis(n_spins, spin_matrix, E, M, exp_de);
+    Metropolis(n_spins, spin_matrix, E, M, exp_de, count_configs);
     REQUIRE(spin_matrix(0,0)+spin_matrix(1,0)*spin_matrix(0,1)+spin_matrix(1,1)=3);
 }
 
