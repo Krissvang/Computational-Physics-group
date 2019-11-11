be60cecf53c15ecf44424cdec0c8010d5c77939a
Tore Klungland <tore.klungland@fys.uio.no>
2019-11-11 22:21:42 +0100

    Updated linear fit to give std.dev. of critical temp.

--- Project4/cpp/finding_crit_temp.py
+++ Project4/cpp/finding_crit_temp.py
@@ -5,7 +5,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def crit_temp():
-  temp, energy40, heatcap40, mag, absmag, sucept, time = np.loadtxt("results/L=40.txt", skiprows=2,unpack=True) 
+  temp, energy40, heatcap40, mag, absmag, sucept, time = np.loadtxt("results/L=40.txt", skiprows=2,unpack=True)
   temp, energy60, heatcap60, mag, absmag, sucept, time = np.loadtxt("results/L=60.txt", skiprows=2,unpack=True)
   temp, energy80, heatcap80, mag, absmag, sucept, time = np.loadtxt("results/L=80.txt", skiprows=2,unpack=True)
   temp, energy100, heatcap100, mag, absmag, sucept, time = np.loadtxt("results/L=100.txt", skiprows=2,unpack=True)
@@ -14,9 +14,21 @@ def crit_temp():
   index_max60=np.argmax(heatcap60)
   index_max80=np.argmax(heatcap80)
   index_max100=np.argmax(heatcap100)
-  y=[temp[index_max100],temp[index_max80],temp[index_max60],temp[index_max40]]
-  slope, crit_temp, r_value, p_value, std_err = stats.linregress(x, y)
+  y=np.array([temp[index_max100],temp[index_max80],temp[index_max60],temp[index_max40]])
+  #slope, crit_temp, r_value, p_value, std_err = stats.linregress(x, y)
+
+  n = len(x)
+  D = sum(x**2)-(1./n)*(sum(x))**2
+  E = sum(x*y)-(1./n)*sum(x)*sum(y)
+  F = sum(y**2)-(1./n)*(sum(y))**2
+  xm = np.mean(x)
+  ym = np.mean(y)
+  m = E/D
+  c = ym-m*xm
+  dm = np.sqrt(1./(n-2)*(D*F-E**2)/D**2)
+  dc = np.sqrt(1./(n-2)*(D/n+xm**2)*(D*F-E**2)/D**2)
   crit_temp_exact=2/np.log(1+np.sqrt(2))
+  slope = m; crit_temp = c; std_err = dc
   print("Calculated critical temperature = ", crit_temp, "STD ERROR ", std_err)
   print("Difference between exact and calulated critical temp: ", abs(crit_temp_exact-crit_temp))
   plt.figure("Plot")
