21cb5c387b2208998104db5f389e5f83eb66ff38
krissvang <kristian.vangsnes@hotmail.com>
2019-12-15 20:44:18 +0100

    Commented and removed unecessary files

--- Project5/cpp/plot.py
+++ Project5/cpp/plot.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Have to use this for unix subsystem
-
+#This program creates the plots needed
 
 
 w= "1"
@@ -15,21 +15,26 @@ def plot_energy_vs_alpha(omega):
   np.loadtxt("results/energy_vs_alpha_w=%s"%(omega), skiprows=2,unpack=True) 
   plt.figure("Plot")
   plt.grid(1)
-  plt.plot(alpha,pot_e,'#E57375',marker='o',linestyle='dashed', markersize='3')
+  plt.plot(alpha,pot_e,'#E57375',marker='o',linestyle='dashed',
+   markersize='3')
   plt.xlabel("$\\alpha$",fontsize="14")
   plt.ylabel("$E_0$",fontsize="14")
   plt.savefig("results/energy_vs_alpha_w=%s.pdf"%(omega))
   plt.close()
 
 def plot_r12_vs_omega(filename,filename2,identifier,identifier2):
-  var, pot_e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, pot_e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,\
+    pe_w,var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename), skiprows=2,unpack=True) 
-  var1, pot_e1, var_pot_e1, kin_e1, var_kin_e1, pe_wo,var_pe_wo1, pe_w,var_pe_w1, r12_1, Accepted1 =\
+  var1, pot_e1, var_pot_e1, kin_e1, var_kin_e1, pe_wo,var_pe_wo1,\
+     pe_w,var_pe_w1, r12_1, Accepted1 =\
   np.loadtxt("results/%s"%(filename2), skiprows=2,unpack=True) 
   plt.figure("Plot")
   plt.grid(1)
-  plt.plot(var,r12,'#E57375',marker='o',linestyle='dashed', markersize='3',label="With Jastrow")
-  plt.plot(var,r12_1,'#4d80e4',marker='o',linestyle='dashed', markersize='3', label="Without Jastrow")
+  plt.plot(var,r12,'#E57375',marker='o',linestyle='dashed',
+   markersize='3',label="With Jastrow")
+  plt.plot(var,r12_1,'#4d80e4',marker='o',linestyle='dashed', 
+  markersize='3', label="Without Jastrow")
 
   plt.legend()
 
@@ -40,17 +45,21 @@ def plot_r12_vs_omega(filename,filename2,identifier,identifier2):
   plt.close()
 
 def plot_energy_vs_omega(filename,filename2):
-  var, e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,\
+    pe_w,var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename), skiprows=2,unpack=True) 
 
-  var, e1, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, e1, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,\
+    pe_w,var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename2), skiprows=2,unpack=True) 
 
   plt.figure("Plot")
   plt.grid(1)
 
-  plt.plot(var,e,'#E57375',marker='o',linestyle='dashed', markersize='3',label="With Jastrow")
-  plt.plot(var,e1,'#4d80e4',marker='o',linestyle='dashed', markersize='3',label="Without Jastrow")
+  plt.plot(var,e,'#E57375',marker='o',linestyle='dashed',
+   markersize='3',label="With Jastrow")
+  plt.plot(var,e1,'#4d80e4',marker='o',linestyle='dashed',
+   markersize='3',label="Without Jastrow")
 
   plt.xlabel("$\omega$",fontsize="14")
   plt.ylabel("$\langle E_L \\rangle$",fontsize="14")
@@ -59,16 +68,20 @@ def plot_energy_vs_omega(filename,filename2):
   plt.close()
 
 def plot_var_vs_omega(filename,filename2):
-  var, e, var_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, e, var_e, kin_e, var_kin_e,pe_wo,var_pe_wo,\
+    pe_w,var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename), skiprows=2,unpack=True) 
 
-  var, e, var_e1, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, e, var_e1, kin_e, var_kin_e,pe_wo,var_pe_wo,\
+    pe_w,var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename2), skiprows=2,unpack=True) 
 
   plt.figure("Plot")
   plt.grid(1)
-  plt.plot(var,var_e,'#E57375',marker='o',linestyle='dashed', markersize='3',label="With Jastrow")
-  plt.plot(var,var_e1,'#4d80e4',marker='o',linestyle='dashed', markersize='3',label="Without Jastrow")
+  plt.plot(var,var_e,'#E57375',marker='o',linestyle='dashed',
+   markersize='3',label="With Jastrow")
+  plt.plot(var,var_e1,'#4d80e4',marker='o',linestyle='dashed',
+   markersize='3',label="Without Jastrow")
 
   plt.xlabel("$\omega$",fontsize="14")
   plt.ylabel("$\sigma^2$",fontsize="14")
@@ -78,30 +91,37 @@ def plot_var_vs_omega(filename,filename2):
 
 
 def plot_kin_vs_pot_wo(filename,identifier):
-  var, e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,var_pe_w, r12, Accepted =\
+  var, e, var_pot_e, kin_e, var_kin_e,pe_wo,var_pe_wo,pe_w,\
+    var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename), skiprows=2,unpack=True) 
   plt.figure("Plot")
   plt.grid(1)
-  plt.plot(var,kin_e/pe_wo,'#E57375',marker='o',linestyle='dashed', markersize='3')
+  plt.plot(var,kin_e/pe_wo,'#E57375',marker='o',linestyle='dashed',
+   markersize='3')
   plt.xlabel("$\omega$",fontsize="14")
-  plt.ylabel("$\\frac{\langle T \\rangle}{\langle V \\rangle}$",fontsize="14")
+  plt.ylabel("$\\frac{\langle T \\rangle}{\langle V \\rangle}$",
+  fontsize="14")
   plt.savefig("results/ke_vs_pe_unperturbed%s.pdf"%(identifier))
   plt.close()
 
 def plot_kin_vs_pot(filename, filename3):
-  var, e, var_pot_e, kin_e, var_kin_e, pe_wo, var_pe_wo, pe_w,var_pe_w, r12, Accepted =\
+  var, e, var_pot_e, kin_e, var_kin_e, pe_wo, var_pe_wo, pe_w,\
+    var_pe_w, r12, Accepted =\
   np.loadtxt("results/%s"%(filename), skiprows=2,unpack=True) 
 
-  var, e, var_pot_e, kin_e3, var_kin_e3, pe_wo3,var_pe_wo3, pe_w3, var_pe_w3, r12, Accepted =\
+  var, e, var_pot_e, kin_e3, var_kin_e3, pe_wo3,var_pe_wo3, pe_w3,\
+     var_pe_w3, r12, Accepted =\
   np.loadtxt("results/%s"%(filename3), skiprows=2,unpack=True) 
 
   plt.figure("Plot")
   plt.grid(1)
 
-  plt.plot(var,kin_e/pe_w,'#E57375',marker='o',linestyle='dashed', markersize='3',label="With repulsion")
+  plt.plot(var,kin_e/pe_w,'#E57375',marker='o',linestyle='dashed',
+   markersize='3',label="With repulsion")
 
 
-  plt.plot(var,kin_e3/pe_wo3,'#4d80e4',marker='o',linestyle='dashed', markersize='3', label = "Without repultion")
+  plt.plot(var,kin_e3/pe_wo3,'#4d80e4',marker='o',linestyle='dashed',
+   markersize='3', label = "Without repultion")
 
   plt.xlabel("$\omega$",fontsize="14")
   plt.ylabel("$\\frac{\langle T \\rangle}{\langle V \\rangle}$",fontsize="14")
